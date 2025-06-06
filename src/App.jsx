8076bd707fa3250204c71ceeb2a03f1b7e3d1a92
Neil <neilmathewsuby@gmail.com>
2025-06-06 05:33:12 +0530

    submit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BirthdayForm from './components/BirthdayForm'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import AddBirthday from './pages/AddBirthday'    // Add this import
-import ViewBirthday from './pages/ViewBirthday'  // Add this import
 
 
 const App = () => {
+  const [birthday, setBirthday] = useState([])
   return (
     <div>
       <div>
-        <Router>
-          <Routes>
-            <Route path = "/" element = {<Home />}/>
-            <Route path="/add" element = {<AddBirthday /> } />
-            <Route  path ="/view" element = {<ViewBirthday />} />
-          </Routes>
-        </Router>
+        <BirthdayForm setBirthday={setBirthday}/>
         
       </div>
     </div>
